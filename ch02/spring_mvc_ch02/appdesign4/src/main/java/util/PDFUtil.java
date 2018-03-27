b20e0482b04849453db4ca64f188862e36313c23
Carlos Villanueva <ceva_19@hotmail.com>
2018-03-27 19:22:00 -0300

    Working with Dependency Injection

---
+++ ch02/spring_mvc_ch02/appdesign4/src/main/java/util/PDFUtil.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+/**
+ *
+ * @author barcvilla
+ */
+public class PDFUtil {
+    public static void createDocument(String path, String input)
+    {
+        PDDocument doc = null;
+        try
+        {
+            doc = new PDDocument();
+            PDFont font = PDType1Font.HELVETICA;
+            PDPage page = new PDPage();
+            doc.addPage(page);
+            float fontSize = 12.0f;
+            
+            PDRectangle pageSize = page.getMediaBox();
+            float centeredXPosition = (pageSize.getWidth() - fontSize / 1000f) / 2f;
+            float stringWidth = font.getStringWidth(input);
+            
+            PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+            contentStream.setFont(font, fontSize);
+            contentStream.beginText();
+            contentStream.moveTextPositionByAmount(centeredXPosition, 600);
+            contentStream.drawString(input);
+            contentStream.endText();
+            contentStream.close();
+            doc.save(path);
+            doc.close();
+        }
+        catch(Exception ex)
+        {   
+            ex.printStackTrace();
+        }
+    }
+}
